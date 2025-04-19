f459cea9dcdb9d060107465876755afb380ff190
Abd-Habeel <abdulhabeellm10@gmail.com>
2025-04-19 15:22:47 +0530

    Added files

---
+++ resources/views/inc/slide.blade.php
@@ -0,0 +1,25 @@
+	
+<div id="slide" class="carousel slide mb-5" data-ride="carousel">
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img class="d-block slide-item" src="{{ asset('image/chmsc1.png')}}" alt="First slide">
+		</div>
+		<div class="carousel-item">
+			<img class="d-block slide-item" src="{{ asset('image/chmsc2.png')}}" alt="Second slide">
+		</div>
+		<div class="carousel-item">
+			<img class="d-block slide-item" src="{{ asset('image/chmsc3.png')}}" alt="Third slide">
+		</div>
+		<div class="carousel-item">
+			<img class="d-block slide-item" src="{{ asset('image/chmsc4.png')}}" alt="Third slide">
+		</div>
+	</div>
+	<a class="carousel-control-prev" href="#slide" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="carousel-control-next" href="#slide" role="button" data-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
